1d99d923efcc755bd71b84618cdcefa09b142e6a
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-11-22 22:32:22 +0600

    Teyrofit data fetching project

---
+++ app/src/main/java/com/example/retrofit_data_fetch/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.retrofit_data_fetch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
